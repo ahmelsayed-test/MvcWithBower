684ff90303f7ab9746c6e7d32a458bd60b8a2f8c
Ahmed ElSayed <ahmels@microsoft.com>
2015-02-18 10:59:43 -0800

    removing not needed stuff

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,21 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 
 namespace MvcWithBower.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: HOme
+        // GET: Home
         public ActionResult Index()
         {
-
+            ViewBag.BowerStatus = System.IO.File.Exists(HostingEnvironment.MapPath(@"~\bower_components\jquery\dist\jquery.js"))
+                ? "Bower was restored"
+                : "bower wasn't restored";
             return View();
         }
     }
