502ba86e141e5b14c2fbf07933b176ee0cd9c6bf
Ahmed ElSayed <ahmels@microsoft.com>
2015-02-18 11:00:31 -0800

    adding bower to deployment script

--- deploy.cmd
+++ deploy.cmd
@@ -88,6 +88,14 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
+:: 4. Bower Install
+if EXIST "%DEPLOYMENT_TARGET%\bower.json" (
+    pushd "%DEPLOYMENT_TARGET%"
+    call :ExecuteCmd bower install
+    IF !ERRORLEVEL! NEQ 0 goto error
+    popd
+)
+
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: Post deployment stub
