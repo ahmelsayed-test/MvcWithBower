577a24e0f1e35631a3e01378587e0d7a9297ab73
Ahmed ElSayed <ahmels@microsoft.com>
2015-02-18 09:53:16 -0800

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWithBower.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: HOme
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
